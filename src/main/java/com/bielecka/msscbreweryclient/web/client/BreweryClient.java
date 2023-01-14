f16d82e8eaba47f9b82423ca5fdcf005fb57de15
Brano Bielecka <branislav.bielecka@gmail.com>
2023-01-14 21:23:49 +0100

    S5E51: added Customer REST calls and tests

--- src/main/java/com/bielecka/msscbreweryclient/web/client/BreweryClient.java
+++ src/main/java/com/bielecka/msscbreweryclient/web/client/BreweryClient.java
@@ -1,6 +1,7 @@
 package com.bielecka.msscbreweryclient.web.client;
 
 import com.bielecka.msscbreweryclient.web.model.BeerDto;
+import com.bielecka.msscbreweryclient.web.model.CustomerDto;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
@@ -14,6 +15,7 @@ import java.util.UUID;
 public class BreweryClient {
 
     public final String BEER_PATH_V1 = "/api/v1/beer/";
+    public final String CUSTOMER_PATH_V1 = "/api/v1/customer/";
     private String apiHost;
     private final RestTemplate restTemplate;
 
@@ -39,5 +41,21 @@ public class BreweryClient {
 
     public void setApiHost(String apiHost) {
         this.apiHost = apiHost;
+    }
+
+    public CustomerDto getCustomerById(UUID uuid) {
+        return restTemplate.getForObject(apiHost + CUSTOMER_PATH_V1 + uuid, CustomerDto.class);
+    }
+
+    public URI saveNewCustomer(CustomerDto customerDto) {
+        return restTemplate.postForLocation(apiHost + CUSTOMER_PATH_V1, customerDto);
+    }
+
+    public void updateCustomer(UUID uuid, CustomerDto customerDto) {
+        restTemplate.put(apiHost + CUSTOMER_PATH_V1 + uuid, customerDto);
+    }
+
+    public void deleteCustomer(UUID uuid) {
+        restTemplate.delete(apiHost + CUSTOMER_PATH_V1 + uuid);
     }
 }
--- src/test/java/com/bielecka/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/com/bielecka/msscbreweryclient/web/client/BreweryClientTest.java
@@ -1,6 +1,7 @@
 package com.bielecka.msscbreweryclient.web.client;
 
 import com.bielecka.msscbreweryclient.web.model.BeerDto;
+import com.bielecka.msscbreweryclient.web.model.CustomerDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -46,5 +47,38 @@ class BreweryClientTest {
     @Test
     void testDeleteBeer() {
         client.deleteBeer(UUID.randomUUID());
+    }
+
+    @Test
+    void testGetCustomerById() {
+        //given
+        CustomerDto dto = client.getCustomerById(UUID.randomUUID());
+
+        assertNotNull(dto);
+    }
+
+    @Test
+    void testSaveNewCustomer() {
+        //given
+        CustomerDto dto = CustomerDto.builder().name("Joe").build();
+
+        URI uri = client.saveNewCustomer(dto);
+
+        assertNotNull(uri);
+
+        System.out.println(uri);
+    }
+
+    @Test
+    void testUpdateCustomer() {
+        //given
+        CustomerDto dto = CustomerDto.builder().name("Jim").build();
+
+        client.updateCustomer(UUID.randomUUID(), dto);
+    }
+
+    @Test
+    void testDeleteCustomer() {
+        client.deleteCustomer(UUID.randomUUID());
     }
 }
